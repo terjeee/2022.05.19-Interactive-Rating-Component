01f40e86fb2c84ff5aafa65e2a97435aec4a77c0
terjeee <96795863+terjeee@users.noreply.github.com>
2022-05-26 04:34:27 +0200

    begynt på submit ++

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
 const btnRating = document.querySelectorAll('.btnRating');
 const btnSubmit = document.querySelector('#btnSubmit');
 
+const page1 = document.querySelector('#page1');
+
 let rating;
 
+// eventListener på btnRating(1-5)
+
 btnRating.forEach(btn =>
-  btn.addEventListener('click', function (event) {
-    rating = event.target.value;
+  btn.addEventListener('click', function (e) {
+    rating = e.target.value;
   })
 );
+
+// eventListener på btnSubmit
+
+btnSubmit.addEventListener('click', function (e) {
+  if (!rating) return;
+
+  page1.classList.add('hidden');
+});
