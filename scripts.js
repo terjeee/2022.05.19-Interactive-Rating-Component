f34f8bcfc2cd41c40a84d98b87faeda4b76a9119
terjeee <96795863+terjeee@users.noreply.github.com>
2022-05-26 02:51:31 +0200

    +eventListener for å extracte verdi (1-5) fra btn

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
 const btnRating = document.querySelectorAll('.btnRating');
 const btnSubmit = document.querySelector('#btnSubmit');
+
+let rating;
+
+btnRating.forEach(btn =>
+  btn.addEventListener('click', function (event) {
+    rating = event.target.value;
+  })
+);
